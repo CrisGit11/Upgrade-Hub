1d743e3b70acdd8589a0c3c3d4ab86fbe447aed4
CrisGit11 <102029775+CrisGit11@users.noreply.github.com>
2025-03-09 17:41:59 +0100

    Update events.route.js

--- API_SPORT_EVENTS/src/api/routes/api/events.route.js
+++ API_SPORT_EVENTS/src/api/routes/api/events.route.js
@@ -7,7 +7,7 @@ const checkToken = require("../../middleware/auth");
 //Declaramos todos los endpoints
 
 router.get("/list", events.gelAll); //Nos devuelve el listado de todos los eventos deportivos (Le he tenido que cambiar el nombre de la ruta ya que tenemos otro metrodo de tipo get con el mismo nombre router.get("/", events.getypeEvents))
-//router.get("/:eventId", events.getEventById); //Nos devuelve los detalles de un evento especifico
+router.get("/:eventId", events.getEventById); //Nos devuelve los detalles de un evento especifico
 router.post("/", checkToken, events.createEvents); //Nos permite crear eventos nuevos
 router.put("/:eventId", checkToken, events.updateEvent); //Nos permite actualizar un evento 
 router.delete("/:eventId", checkToken, events.deleteEvent); //Elimina un evento específico
